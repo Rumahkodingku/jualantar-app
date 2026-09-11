6bfac737c229ea499f58ce6039dcac659d7215bd
xRiot45 <tomasalberto527@gmail.com>
2026-09-11 19:55:17 +0700

    chore: running bun format

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "vite"
 
 export default defineConfig({
-  resolve: { tsconfigPaths: true },
-  plugins: [tailwindcss(), reactRouter()],
+    resolve: { tsconfigPaths: true },
+    plugins: [tailwindcss(), reactRouter()],
 })
